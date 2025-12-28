f401a97d258c533e0c1757065b49e6ddb6493ab1
saruabh rajput <saurabhraj2509@gmail.com>
2025-12-29 02:53:05 +0530

    Add interviewplatfrom package and update dependencies in backend and frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
+const __dirname = path.resolve();
 
 const app = express();
-
 app.get('/', (req, res)=>{
     res.status(200).json({msg: "success from api"});
+
 })
 
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/{*any}", (req, res)=> {res.sendFile(
+        path.join(__dirname, "../frontend", "dist", "index.html"));
+})
+}
 app.listen(ENV.PORT, ()=> console.log("server is running on port : "+ ENV.PORT));
