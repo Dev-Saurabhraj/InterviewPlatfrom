ecb0d6741b3a884594c6377a47dedd48a2d2a908
saruabh rajput <saurabhraj2509@gmail.com>
2026-01-11 19:17:28 +0530

    some changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import cors from "cors";
-import serve from "inngest/express"
+import {serve} from "inngest/express"
 import {inngest, functions} from "./lib/inngest.js"
 import { connectDB } from "./lib/db.js";
 const __dirname = path.resolve();
