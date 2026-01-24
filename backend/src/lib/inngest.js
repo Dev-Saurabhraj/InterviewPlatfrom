366e59a3bedd8583aa8b3aceb375cfb44ca430c0
saurabh <saurabhraj2509@gmail.com>
2026-01-24 17:24:12 +0530

    stream auth sync

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,26 +1,39 @@
 import {Inngest } from "inngest"
 import {connectDB} from "./db.js"
 import { User } from "../models/User.js"
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({id:'project-inngest'});
 const syncUser = inngest.createFunction(
 {
     id: "sysnc-user"
 },
-{
+{ 
     event:"clerk/user.created",
 },
 
 async ({event})=>{
     await connectDB()
+
+    //  geting the data from clerk
     const {id, email_addresses, first_name, last_name, image_url} = event.data
+
     const newUser = {
         clerkId:id,
         email:email_addresses[0]?.email_address,
         name: `${first_name || " "} ${last_name||" "}`,
         profileImage: image_url
     }
+
+    // createing new user in mongo data base
     await User.create(newUser)
+
+    // createing new user in stream app;
+    await upsertStreamUser({
+        id: newUser.clerkId.toString(),
+        name: newUser.name,
+        image: newUser.profileImage
+    })
 }
 
 )
@@ -32,7 +45,9 @@ const deleteUser = inngest.createFunction(
         await connectDB();
         const {id} = event.data
         await User.deleteOne({clerkId: id});
+        await deleteStreamUser(id.toString());
     }
+
 )
 
 
