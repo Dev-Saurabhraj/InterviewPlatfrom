7d62fe256e837bf361b1cf41cc70e4a1541bfeb8
saurabh <saurabhraj2509@gmail.com>
2026-01-24 17:53:34 +0530

    added stream chat dependency

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,12 +4,12 @@ import { ENV } from "./env.js"
 export const connectDB = async()=>{
     try {
         if(!ENV.DB_URL) {
-            throw new Error("DB_url not defined in the enviroment variables");
+            throw new Error("DB_url is not defined in the enviroment variables");
         }
         const conn = await mongoose.connect(ENV.DB_URL);
         console.log("connected to database ", conn.connection.host);
     } catch (error) {
-        console.error("erro connecting to mongoD", error);
+        console.error("db.js : error connecting to mongoD", error);
         process.exit(1) // 0 means success, 1 means failure
     }
 }
